96fea2b0926576bfb6e8773c39d876c6bbd87776
haotien91 <101621731+haotien91@users.noreply.github.com>
2024-01-26 19:41:13 +0800

    Update lab01.sh

--- lab01/lab01.sh
+++ lab01/lab01.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#PBS -N pi-mt-Intel-OMPI
-#PBS -P ACD110018
-#PBS -q cf160
-#PBS -o out.log
-#PBS -e err.log
+#PBS -N pi-OMPI
+#PBS -P ACD112218
+#PBS -q ct160
 #PBS -l mpiprocs=$(TODO)
 
-# Please set TRIES to 100,000 and experiment with different numbers of processes (NUM_OF_PROCS).
+# Please set TRIES to 1,000,000,000 and experiment with different numbers of processes (NUM_OF_PROCS).
 NUM_OF_PROCS=$(TODO)
-TRIES=100000
+TRIES=1000000000
 
 module purge
 module load gcc/9.3.0
@@ -18,6 +16,6 @@ cd ./SC25-WinterCamp-PBS
 
 make clean && make
 
-echo "Calculat pi by Monte Carlo method with 100000 tries!!"
+echo "Calculat pi by Monte Carlo method with 1,000,000,000 tries!!"
 
 mpirun --mca btl ^openib -np $NUM_OF_PROCS ./mpi-monte-carlo.run $TRIES
